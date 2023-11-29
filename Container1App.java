375b770b19c63649be8a555add962b934cacca9a
Rahul Vishwakarma <rahulvishwakarma1181@gmail.com>
2023-11-29 12:38:36 +0530

    Update Container1App.java

--- Container1App.java
+++ Container1App.java
@@ -3,7 +3,7 @@ import java.net.*;
 public class Container1App {
     public static void main(String[] args) throws Exception {
         DatagramSocket socket = new DatagramSocket();
-        byte[] sendData = "Hello from Container 1".getBytes();
+        byte[] sendData = "Hello, Rahul here!".getBytes();
         InetAddress receiverAddress = InetAddress.getByName("container2");
 
         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, receiverAddress, 9876);
